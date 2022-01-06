f297f6a5714e46f0ff79470fc25a6c758faa69d3
pvicugna11 <pvicugna11@gmail.com>
2022-01-06 17:11:14 +0900

    Scene management

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -9,10 +10,11 @@ using UnityEngine;
 public class MenuUIHandler : MonoBehaviour
 {
     public ColorPicker ColorPicker;
+    public static Color cubeColor = new Color(0f, 1f, 1f, 1f);
 
     public void NewColorSelected(Color color)
     {
-        // add code here to handle when a color is selected
+        cubeColor = color;
     }
     
     private void Start()
@@ -20,5 +22,25 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SaveColorClicked()
+    {
+
+    }
+
+    public void LoadColorCliked()
+    {
+
+    }
+
+    public void Exit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 }
