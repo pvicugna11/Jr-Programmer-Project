4ffd40ee204a8df6faceac7414e3e90f89e3f9d1
pvicugna11 <pvicugna11@gmail.com>
2022-01-07 20:14:15 +0900

    Inheritance and polymorphism
    本当は近くにいるときだけ生産量を増加させたかった．

--- Assets/Scripts/ProductivityUnit.cs
+++ Assets/Scripts/ProductivityUnit.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class ProductivityUnit : Unit
 {
+    public float ProductionSpeedToMultiply = 2f;
+
+    private ResourcePile m_CurrentTransportTarget;
+
     // We override the GoTo function to remove the current transport target, as any go to order will cancel the transport
     public override void GoTo(Vector3 position)
     {
         base.GoTo(position);
     }
     
-    protected override void BuildingInRange() {}
+    protected override void BuildingInRange()
+    {
+        ResourcePile rp = (m_Target is ResourcePile resourcePile) ? resourcePile : null;
+        if (rp != m_CurrentTransportTarget)
+        {
+            if (m_CurrentTransportTarget) m_CurrentTransportTarget.ProductionSpeed /= ProductionSpeedToMultiply;
+            rp.ProductionSpeed *= ProductionSpeedToMultiply;
+            m_CurrentTransportTarget = rp;
+        }
+    }
     
     //Override all the UI function to give a new name and display what it is currently transporting
     public override string GetName()
@@ -20,7 +33,7 @@ public class ProductivityUnit : Unit
 
     public override string GetData()
     {
-        return $"Can move but has no further functionality";
+        return $"Increases resource item production in a base";
     }
 
     public override void GetContent(ref List<Building.InventoryEntry> content) {}
