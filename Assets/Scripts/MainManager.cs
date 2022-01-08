0349e85f17eef5c6c970bfa5aac61a648a95e7c8
pvicugna11 <pvicugna11@gmail.com>
2022-01-08 14:30:44 +0900

    Implement data persistence between sessions

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -20,5 +21,43 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        LoadColor();
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        // JSONに変換
+        string json = JsonUtility.ToJson(data);
+
+        // 文字列をファイルに書き出す
+        // 第一引数は，ファイルへのパス；永続的なデータディレクトリにsavefile.jsonというファイルを追加
+        // Application.persistentDataPath：https://docs.unity3d.com/ja/2019.4/ScriptReference/Application-persistentDataPath.html
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        // ファイルが存在するかどうかをチェックする
+        if (File.Exists(path))
+        {
+            // ファイルの内容を読む
+            string json = File.ReadAllText(path);
+            // テキストをSaveDataのインスタンスに変換し直す．
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
     }
 }
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -22,20 +22,11 @@ public class MenuUIHandler : MonoBehaviour
     
     private void Start()
     {
-        LoadCubeColor();
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
-    }
 
-    private void LoadCubeColor()
-    {
-        float r = PlayerPrefs.GetFloat("RED", 0f);
-        float g = PlayerPrefs.GetFloat("GREEN", 1f);
-        float b = PlayerPrefs.GetFloat("BLUE", 1f);
-        float a = PlayerPrefs.GetFloat("ALPHA", 1f);
-
-        MainManager.Instance.TeamColor = new Color(r, g, b, a);
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartNew()
@@ -45,20 +36,19 @@ public class MenuUIHandler : MonoBehaviour
 
     public void SaveColorClicked()
     {
-        PlayerPrefs.SetFloat("RED", MainManager.Instance.TeamColor.r);
-        PlayerPrefs.SetFloat("GREEN", MainManager.Instance.TeamColor.g);
-        PlayerPrefs.SetFloat("BLUE", MainManager.Instance.TeamColor.b);
-        PlayerPrefs.SetFloat("ALPHA", MainManager.Instance.TeamColor.a);
-        PlayerPrefs.Save();
+        MainManager.Instance.SaveColor();
     }
 
     public void LoadColorCliked()
     {
-        LoadCubeColor();
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void Exit()
     {
+        MainManager.Instance.SaveColor();
+
 // 条件付きコンパイル
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
